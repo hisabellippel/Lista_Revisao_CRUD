f58e4a30c48e4a561d7556bc12cd8954fecc29f7
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-10-30 08:57:14 -0300

    add - banco, diagramas

---
+++ banco.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE kanban_db;
+USE kanban_db;
+
+CREATE TABLE usuarios (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(120) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tarefas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(100) NOT NULL,
+    setor VARCHAR(100) NOT NULL,
+    prioridade ENUM('Baixa', 'Média', 'Alta') NOT NULL,
+    data_cadastro DATE NOT NULL,
+    status_tarefa ENUM('Fazer', 'Fazendo', 'Pronto') NOT NULL,
+    usuario_responsavel INT NOT NULL,
+    FOREIGN KEY (usuario_responsavel) REFERENCES usuarios(id)
+);
+
+INSERT INTO usuarios (nome, email) VALUES
+('Hisabel Lippel', 'hisabel@lippel');
+
+INSERT INTO tarefas (descricao, setor, prioridade, data_cadastro, status_tarefa, usuario_responsavel) VALUES
+('Guardar os refrigerantes', 'Bebidas', 'Média', '2025-09-10', 'Fazer', 1);
