af9dd0f9013865a7b606f9da02bf133e6e8dfaf6
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-10-30 09:42:21 -0300

    add - alterações no style e criação de tela de cadastro de tarefas

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Gerenciamento de Tarefas</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <header>
+        <h1>Sistema de Gerenciamento de Tarefas</h1>
+    </header>
+
+<main>
+    <div class="menu-container">
+      <a href="public/create_usuarios.php">
+        <button class="botaoMenu">
+          <h4>Cadastro de Usuários</h4>
+        </button>
+      </a>
+    </div>
+
+    <div class="menu-container">
+      <a href="public/create_tarefas.php">
+        <button class="botaoMenu">
+          <h4>Cadastrar Tarefas</h4>
+        </button>
+      </a>
+    </div>
+</main>
+
+</body>
+</html>
